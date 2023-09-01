7830c803b2d537367e524e422785ca7495f7cf07
unknown <fadeeshamala@gmail.com>
2023-09-01 23:56:35 +0300

    test.v19

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index($name , $age){
+
+        return view('index', compact('name','age'));
+    }
+}
